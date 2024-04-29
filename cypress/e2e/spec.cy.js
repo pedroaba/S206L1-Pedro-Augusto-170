4526e02191360bec56b6b7c6667dd11f673eaaa5
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-04-28 22:23:17 -0300

    Aula 03

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,85 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference = cypress>
+
+describe('Teste de criação, registro e login', () => {
+  it('Teste criação de usuário com sucesso', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type("Pedro")
+    cy.get('#Text1').type("Pedro")
+    cy.get('#username').type("Pedro")
+    cy.get('#password').type("Pedro")
+    cy.get('.btn-primary').click()
+
+    cy.get('.ng-binding').should('contain.text', 'Registration successful')
+  });
+  
+  it('Teste criação de usuário com falha', () => {
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type("Pedro")
+    cy.get('#Text1').type("Pedro")
+    cy.get('#username').type("Pedro")
+    cy.get('.btn-primary').should('be.disabled')
+  });
+
+  it('Teste de login com sucesso', () => {
+    const { password, userId } = registerUser()
+
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('#username').type(userId)
+    cy.get('#password').type(password)
+
+    cy.get('.btn-primary').click()
+
+    cy.get('h1.ng-binding').should('contain.text', userId)
+  });
+
+  it('Teste de login depois de deletar o usuário', () => {
+    const { userId, password } = makeLogin()
+    
+    cy.get('.ng-binding > a').click()
+    cy.get('.btn').should('be.visible').click()
+
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('#username').type(userId)
+    cy.get('#password').type(password)
+
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should('have.text', 'Username or password is incorrect')
+  });
+    
+});
+
+function registerUser() { 
+  const now = Date.now()
+  const userId = `${now}ID`
+  const password = `${now}PASSWORD`
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+  cy.get('.btn-link').click()
+  cy.get('#firstName').type(userId)
+  cy.get('#Text1').type(userId)
+  cy.get('#username').type(userId)
+  cy.get('#password').type(password)
+  cy.get('.btn-primary').click()
+
+  return {
+    userId,
+    password
+  }
+}
+
+function makeLogin() {
+  const { password, userId } = registerUser()
+
+  cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+  cy.get('#username').type(userId)
+  cy.get('#password').type(password)
+
+  cy.get('.btn-primary').click()
+
+  return {
+    userId,
+    password
+  }
+}
